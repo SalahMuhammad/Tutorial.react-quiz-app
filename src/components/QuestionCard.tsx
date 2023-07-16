4ed49cfd6f3692d438b03165a289088cd156d15d
SalahMuhammad <salahnazeer93@gmail.com>
2023-07-16 18:39:33 +0300

    main

---
+++ src/components/QuestionCard.tsx
@@ -0,0 +1,48 @@
+import React from "react";
+// types
+import { AnswerObject } from "../App";
+
+interface props {
+  question: string;
+  answers: string[];
+  callback: (e: React.MouseEvent<HTMLButtonElement>) => void;
+  userAnswer: AnswerObject | undefined;
+  questionNr: number;
+  totalQuestions: number;
+}
+
+const QuestionCard: React.FC<props> = ({
+  question,
+  answers,
+  callback,
+  userAnswer,
+  questionNr,
+  totalQuestions,
+}) => {
+  return (
+    <div>
+      <p className="number">
+        Question: {questionNr} / {totalQuestions}
+      </p>
+      <p dangerouslySetInnerHTML={{ __html: question }} />
+      <div>
+        {answers.map((answer) => (
+          <div key={answer}>
+            {/* !!userAnswer convert it to boolean value */}
+            <button
+              disabled={userAnswer ? true : false}
+              value={answer}
+              onClick={callback}
+            >
+              <span dangerouslySetInnerHTML={{ __html: answer }} />
+            </button>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+// malicious
+
+export default QuestionCard;
